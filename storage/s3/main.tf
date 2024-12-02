53d575fbcf95cbbadb5a03ee0c7b4b9d9360255a
Digbijayinee Clara <52991229+DigbijayineeClara07@users.noreply.github.com>
2024-12-02 14:25:37 +0530

    Update main.tf

--- storage/s3/main.tf
+++ storage/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "new-test-bucket-${random_string.suffix.result}"
+  bucket = "new-test-bucket-123456789987654345676567543"
 
   tags = {
     Name        = "My bucket"
