53979ce6536bcc69d788808e7510fdd84a130995
Digbijayinee Clara <52991229+DigbijayineeClara07@users.noreply.github.com>
2024-12-02 13:32:34 +0530

    Create variables.tf

---
+++ compute/lambda/variables.tf
@@ -0,0 +1,44 @@
+variable "region" {
+   type = string
+ }
+
+ variable "lambdas" {
+   description = "A map of Lambda configurations"
+   type = map(object({
+     function_name         = string
+     image_uri             = string
+     log_group             = string
+     role                  = string
+     environment_variables = map(string)
+     memory_size           = number
+     timeout               = number
+   }))
+ }
+
+ variable "ephemeral_storage_size" {
+   description = "The ephemeral storage size for the Lambda function"
+   type        = number
+   default     = 512
+ }
+
+ variable "reserved_concurrent_executions" {
+   description = "The reserved concurrent executions for the Lambda function"
+   type        = string
+   default     = "-1"
+ }
+
+ variable "skip_destroy" {
+   description = "Whether to skip destroy when removing the function"
+   type        = string
+   default     = "false"
+ }
+
+ variable "log_format" {
+   type = string #Select either Text or JSON
+   default = "Text"
+ }
+
+ variable "package_type" {
+   type = string
+   default = "Image"
+ } 
